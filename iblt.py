078aeff604c16fdb68cfe7fa5c5f58b260f4f9f6
Jesper Borgstrup <jesper@borgstrup.dk>
2014-08-29 10:37:30 +0200

    More complete IBLT and tests

--- iblt.py
+++ iblt.py
@@ -34,37 +34,66 @@ class IBLT:
 	key_size = None
 	# value_size is maximum size for values
 	value_size = None
+	# hash_key_sum_size is amount of bytes used for the hashkeySum field
+	hash_key_sum_size = None
 	# hash is function( i, value ), where i is index of hash function
 	# and value is value to be hashed
 	hash = None
 
-	def __init__( self, m, k, key_size, value_size, hash=None ):
+	empty_key_array = None
+	empty_hash_sum_array = None
+
+	RESULT_GET_NO_MATCH = "no_match"
+	RESULT_GET_MATCH = "match"
+	RESULT_GET_DELETED_MATCH = "deleted_match"
+	RESULT_GET_INCONCLUSIVE = "inconclusive"
+	RESULT_LIST_ENTRIES_COMPLETE = "complete"
+	RESULT_LIST_ENTRIES_INCOMPLETE = "incomplete"
+
+	def __init__( self, m, k, key_size, value_size, hash_key_sum_size=10, hash=None ):
 		"""
 		m is the amount of cells in underlying lookup table
 		k is the amount of hash functions
 		key_size is maximum size for keys
 		value_size is maximum size for values
+		hash_key_sum_size is amount of bytes used for the hashkeySum field
 		hash is function( i, value ), where i is index of hash function
 		    and value is value to be hashed (or None for default hash functions)
 		"""
 		self.m = m
 		self.k = k
-		self.T = [[0,[0 for j in range( key_size )],[0 for j in range( value_size )]] for i in range( m )]
 		self.key_size = key_size
 		self.value_size = value_size
+		self.hash_key_sum_size = hash_key_sum_size
 		self.hash = hash if hash is not None else self.__hash
+		self.T = [[0,[0 for j in range( key_size )],[0 for j in range( value_size )],[0 for j in range( hash_key_sum_size )]] for i in range( m )]
+		self.empty_key_array = [0 for i in range( self.key_size )]
+		self.empty_hash_sum_array = [0 for i in range( self.hash_key_sum_size )]
 
 	def insert( self, key, value ):
+		"""
+		Insert the key/value pair into the IBLT.
+		No return value.
+		"""
+		return self.__insert( self.T, key, value )
+
+	def __insert( self, T, key, value ):
 		indices = set( [self.hash( i, key ) for i in range( self.k ) ] )
 		for index in indices:
 			# Increase count
-			self.T[index][0] += 1
+			T[index][0] += 1
 			# Add key to keySum
-			self.T[index][1] = self.__sum_int_arrays( self.T[index][1], self.__value_to_int_array( key, self.key_size ) )
+			T[index][1] = self.__sum_int_arrays( T[index][1], self.__value_to_int_array( key, self.key_size ) )
 			# Add value to valueSum
-			self.T[index][2] = self.__sum_int_arrays( self.T[index][2], self.__value_to_int_array( value, self.value_size ) )
+			T[index][2] = self.__sum_int_arrays( T[index][2], self.__value_to_int_array( value, self.value_size ) )
+			# Add key hash to hashkeySum
+			T[index][3] = self.__sum_int_arrays( T[index][3], self.__value_to_int_array( IBLT.get_key_hash( key ), self.hash_key_sum_size ) )
 
 	def delete( self, key, value ):
+		"""
+		Delete a key/value pair from the IBLT.
+		No return value.
+		"""
 		return self.__delete( self.T, key, value )
 
 	def __delete( self, T, key, value ):
@@ -74,26 +103,50 @@ class IBLT:
 		for index in indices:
 			# Decrease count
 			T[index][0] -= 1
-			# Add key to keySum
+			# Subtract key from keySum
 			T[index][1] = self.__diff_int_arrays( T[index][1], key_array )
-			# Add value to valueSum
+			# Subtract value from valueSum
 			T[index][2] = self.__diff_int_arrays( T[index][2], value_array )
+			# Subtract key hash from hashkeySum
+			T[index][3] = self.__diff_int_arrays( T[index][3], self.__value_to_int_array( IBLT.get_key_hash( key ), self.hash_key_sum_size ) )
 
 	def get( self, key ):
+		"""
+		Try to get a value from the table with the given key.
+		This function can return four different responses:
+
+		( IBLT.RESULT_GET_NO_MATCH, None ): The key was definitively not in the table
+		( IBLT.RESULT_GET_MATCH, <Value> ): The key was in the table and the value is returned
+		( IBLT.RESULT_GET_DELETED_MATCH, <Value> ): The key was deleted without being inserted
+		( IBLT.RESULT_GET_INCONCLUSIVE, None ): It couldn't be determined if the key was in the table or not
+		"""
 		indices = set( [self.hash( i, key ) for i in range( self.k ) ] )
 		for index in indices:
-			if self.T[index][0] == 0:
-				return None
-			elif self.T[index][0] == 1:
-				if self.T[index][1] == self.__value_to_int_array( key, self.key_size ):
-					return self.__int_array_to_value( self.T[index][2] )
-				else:
-					return None
-		return "not found"
+			if self.T[index][0] == 0 and \
+					self.T[index][1] == self.empty_key_array and \
+					self.T[index][3] == self.empty_hash_sum_array:
+				return ( IBLT.RESULT_GET_NO_MATCH, None )
+			elif self.T[index][0] == 1 and \
+					self.T[index][1] == self.__value_to_int_array( key, self.key_size ) and \
+					self.T[index][3] == self.__value_to_int_array( IBLT.get_key_hash( key ), self.hash_key_sum_size ):
+				return ( IBLT.RESULT_GET_MATCH, self.__int_array_to_value( self.T[index][2] ) )
+			elif self.T[index][0] == -1 and \
+					self.T[index][1] == self.__negate_int_array( self.__value_to_int_array( key, self.key_size ) )and \
+					self.T[index][3] == self.__negate_int_array( self.__value_to_int_array( IBLT.get_key_hash( key ), self.hash_key_sum_size ) ):
+				return ( IBLT.RESULT_GET_DELETED_MATCH, self.__int_array_to_value( self.__negate_int_array( self.T[index][2] ) ) )
+		return ( IBLT.RESULT_GET_INCONCLUSIVE, None )
 
 	def list_entries( self ):
+		"""
+		Tries to list all entries in the table.
+
+		Returns ( <Result>, [<Normal entries>], [<Deleted entries>] )
+		where <Result> is either IBLT.RESULT_LIST_ENTRIES_COMPLETE to indicate that the list is complete,
+		or IBLT.RESULT_LIST_ENTRIES_INCOMPLETE to indicate that some entries couldn't be recovered
+		"""
 		T = deepcopy( self.T )
-		result = []
+		entries = []
+		deleted_entries = []
 		# Inefficient worst case O(n^2) loop, the paper says
 		# "It is a fairly straightforward exercise to implement this method in O(m) time, say, by
 		# using a link-list-based priority queue of cells in T indexed by their count fields and
@@ -101,21 +154,44 @@ class IBLT:
 		while True:
 			for i in range( len( T ) ):
 				entry = T[i]
-				if entry[0] == 1:
-					key = self.__int_array_to_value( entry[1] )
-					value = self.__int_array_to_value( entry[2] )
-
-					result.append( ( key, value ) )
-					self.__delete( T, key, value )
-					break
+				if entry[0] == 1 or entry[0] == -1:
+					if entry[0] == 1 and \
+							entry[3] == self.__value_to_int_array( IBLT.get_key_hash( self.__int_array_to_value( entry[1] ) ), self.hash_key_sum_size ):
+						key = self.__int_array_to_value( entry[1] )
+						value = self.__int_array_to_value( entry[2] )
+						entries.append( ( key, value ) )
+						self.__delete( T, key, value )
+						break
+					elif entry[0] == -1 and \
+							self.__negate_int_array( entry[3] ) == self.__value_to_int_array( IBLT.get_key_hash( self.__int_array_to_value( self.__negate_int_array( entry[1] ) ) ), self.hash_key_sum_size ):
+						key = self.__int_array_to_value( self.__negate_int_array( entry[1] ) )
+						value = self.__int_array_to_value( self.__negate_int_array( entry[2] ) )
+						deleted_entries.append( ( key, value ) )
+						self.__insert( T, key, value )
+						break
+					elif entry[0] == -1:
+						print "********************************************************************"
+						print "Found count: %d" % entry[0]
+						print entry[3]
+						print self.__negate_int_array( entry[3] )
+						print
+						print entry[1]
+						print self.__negate_int_array( entry[1] )
+						print self.__int_array_to_value( self.__negate_int_array( entry[1] ) )
+						print IBLT.get_key_hash( self.__int_array_to_value( self.__negate_int_array( entry[1] ) ) )
+						print self.__value_to_int_array( IBLT.get_key_hash( self.__int_array_to_value( self.__negate_int_array( entry[1] ) ) ), self.hash_key_sum_size )
 			else:
 				break
 
 		if any( filter( lambda e: e[0] != 0, T ) ):
-			return ( "incomplete", result )
-		return ( "complete", result )
+			return ( IBLT.RESULT_LIST_ENTRIES_INCOMPLETE, entries, deleted_entries )
+		return ( IBLT.RESULT_LIST_ENTRIES_COMPLETE, entries, deleted_entries )
 
 	def is_empty( self ):
+		"""
+		Returns true if the table is completely empty, i.e. no contains no entries,
+		inserted or deleted
+		"""
 		return all( map( lambda e: e[0] == 0, self.T ) )
 
 	def dump( self ):
@@ -134,8 +210,11 @@ class IBLT:
 			print "%d: %s" % ( i, e )
 		print "DUMP END::::::::::::::::::::::::"
 
-	hash_hex_length = None
+	@staticmethod
+	def get_key_hash( key ):
+		return hashlib.sha512( key ).digest()
 
+	hash_hex_length = None
 	def __hash( self, i, value ):
 		if self.hash_hex_length == None:
 			# Find the shortest length to extract from the hash,
@@ -164,3 +243,6 @@ class IBLT:
 
 	def __int_array_to_value( self, arr ):
 		return "".join( [ chr(i) for i in arr if i != 0 ] )
+
+	def __negate_int_array( self, arr ):
+		return map( lambda i: (256-i) % 256, arr )
--- test.py
+++ test.py
@@ -34,15 +34,55 @@ assert not t.is_empty()
 t.delete( "testkey", "testvalue" )
 assert t.is_empty()
 
+
 # Test if inserting 10 key/value pairs can be listed out
 pairs = [( "key%d" % i, "value%d" % i ) for i in range( 10 )]
 for key, value in pairs:
 	t.insert( key, value )
 entries = t.list_entries()
-assert entries[0] == 'complete'
+assert entries[0] == IBLT.RESULT_LIST_ENTRIES_COMPLETE
 # Get set intersection, should contain all elements from pairs
 intersect = set(pairs) & set(entries[1])
 assert set( pairs ) == intersect
 # Get set union, should contain only the elements from pairs
 union = set(pairs) | set(entries[1])
 assert set( pairs ) == union
+
+
+# Test if deleting a key/value pair that hasn't been inserted before
+# doesn't result in bad list_entries() or get()
+t = IBLT( 30, 4, 10, 10 )
+t.delete( "delkey", "delval" )
+t.insert( "inskey", "insval" )
+entries = t.list_entries()
+assert entries[0] == IBLT.RESULT_LIST_ENTRIES_COMPLETE
+# One inserted entry
+assert len( entries[1] ) == 1
+assert entries[1][0] == ( "inskey", "insval" )
+# One deleted entry
+assert len( entries[2] ) == 1
+assert entries[2][0] == ( "delkey", "delval" )
+assert t.get( "inskey" ) == ( IBLT.RESULT_GET_MATCH, "insval" )
+assert t.get( "delkey" ) == ( IBLT.RESULT_GET_DELETED_MATCH, "delval" )
+
+
+# Test if inserting more that m=30 keys/values will result in an incomplete listing
+# and then remove some of them again to get a complete listing
+t = IBLT( 30, 4, 10, 10 )
+pairs = [( "key%d" % i, "value%d" % i ) for i in range( 31 )]
+for key, value in pairs:
+	t.insert( key, value )
+
+assert t.list_entries()[0] == IBLT.RESULT_LIST_ENTRIES_INCOMPLETE
+pairs_to_keep = pairs[:15]
+pairs_to_delete = pairs[15:]
+for key, value in pairs_to_delete:
+	t.delete( key, value )
+
+entries = t.list_entries()
+assert entries[0] == IBLT.RESULT_LIST_ENTRIES_COMPLETE
+assert len( entries[1] ) == len( pairs_to_keep )
+# Check if returned entries are the same as in pairs_to_keep
+assert len( set( entries[1] ).difference( set( pairs_to_keep ) ) ) == 0
+# Check that returned entries contains none of the pairs_to_delete
+assert len( set( entries[1] ).intersection( set( pairs_to_delete ) ) ) == 0
